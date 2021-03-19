2a2c4d55c4e023284b226cd408c8cd55eed74b57
surajPanayanchery <suraj.panayanchery@gmail.com>
2021-03-19 08:44:47 +0530

    Added Configuration

--- src/ISDCallAnalaysis/Driver.java
+++ src/ISDCallAnalaysis/Driver.java
@@ -1,9 +1,14 @@
 package ISDCallAnalaysis;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+
 public class Driver {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args)throws Exception {	
+		Configuration conf = new Configuration();
+		Job job = new Job(conf,"My Word Count Program");
 		System.out.println("First Program");
-	}
+	}	
 
 }
